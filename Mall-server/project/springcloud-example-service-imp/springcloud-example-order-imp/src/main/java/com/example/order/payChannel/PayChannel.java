8303554ec48b1c93a2d82a343778b4486ef8d493
WuHao <929732982@qq.com>
2021-01-09 13:04:45 +0800

    remove directory:/Mall-Server in master branch

--- Mall-server/project/springcloud-example-service-imp/springcloud-example-order-imp/src/main/java/com/example/order/payChannel/PayChannel.java
+++
@@ -1,9 +0,0 @@
-package com.example.order.payChannel;
-
-import com.example.entitity.DO.PayTransactionDO;
-
-import javax.servlet.http.HttpServletResponse;
-
-public interface PayChannel {
-    void pay(PayTransactionDO payTransactionDO, HttpServletResponse response);
-}
--- Mall-server/project/springcloud-example-service-imp/springcloud-example-seckill-imp/src/main/java/com/example/seckill/feign/goods/GoodsFeign.java
+++
@@ -1,9 +0,0 @@
-package com.example.seckill.feign.goods;
-
-
-import com.example.api.GoodsService;
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "spring-cloud-goods")
-public interface GoodsFeign extends GoodsService {
-}
