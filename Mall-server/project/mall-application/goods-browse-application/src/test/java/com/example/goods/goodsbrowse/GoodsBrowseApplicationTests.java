41be931753881270d493577d4b8c525e7a29328e
WuHao <929732982@qq.com>
2021-02-25 15:04:21 +0800

    - 更改maven打包配置 - 移除了一些未使用的依赖

--- Mall-server/project/mall-application/goods-browse-application/src/test/java/com/example/goods/goodsbrowse/GoodsBrowseApplicationTests.java
+++ Mall-server/project/mall-application/goods-browse-application/src/test/java/com/example/goods/goodsbrowse/GoodsBrowseApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.goods.goodsbrowse;
 
+import com.example.goods.goodsbrowseapplication.GoodsBrowseApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GoodsBrowseApplication.class)
 class GoodsBrowseApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(1);
     }
 
 }
